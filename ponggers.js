b66925706d56cac72d3e6cbef0a2f45845808506
xanalem <bnavone@gmail.com>
2018-04-29 11:10:25 -0700

    test

--- ponggers.js
+++ ponggers.js
@@ -1,4 +1,4 @@
- function Box(xpos, ypos, xstep, ystep, color, id) {
+function Box(xpos, ypos, xstep, ystep, color, id) {
     this.xpos = xpos;
     this.ypos = ypos;
     this.xstep = xstep;
@@ -37,20 +37,7 @@ for (var i = 0; i < numBoxes; i++) {
 
 var id = setInterval(frame, 5);
 
-function frame() {
-    for (var i = 0; i < boxes.length; i++) {
-        if (boxes[i].xpos > 780 || boxes[i].xpos < 0) {
-            boxes[i].xstep = -boxes[i].xstep;
-        }
-        if (boxes[i].ypos > 580 || boxes[i].ypos < 0) {
-            boxes[i].ystep = -boxes[i].ystep;
-        }
 
-        boxes[i].xpos = boxes[i].xpos + boxes[i].xstep;
-        boxes[i].ypos = boxes[i].ypos + boxes[i].ystep;
-        boxes[i].render();
-    }
-}
 
 //---------------------------------------------------------//
 
@@ -76,61 +63,70 @@ var container = document.getElementById("container");
 for (var i = 0; i < numPaddles; i++) {
     var elem = document.createElement("div");
     elem.className = "paddle";
-    elem.id = "paddle" + i;
+    elem.id = "paddle" ;
     container.appendChild(elem);
-    var rect1 = new Paddle(
+    paddlez[i] =
+     new Paddle(
         0,
         240,
         0,
         1,
         '#' + "FFFFFF",
         elem.id);
-    rect1.render();
 
 }
 
-
+var paddles = [];
+var numPaddles = 1;
 for (var i = 0; i < numPaddles; i++) {
     var elem = document.createElement("div");
     elem.className = "paddle2";
-    elem.id = "paddle2" + i;
+    elem.id = "paddle2" ;
     container.appendChild(elem);
-    var rect2 = new Paddle(
+    paddles[i] =
+     new Paddle(
         783,
         240,
         0,
         1,
         '#' + "FFFFFF",
         elem.id);
-    rect2.render();
 
 }
 
-function frame1() {
+function frame() {
     for (var i = 0; i < boxes.length; i++) {
-        if (rect1.x < boxes[i].x + boxes[i].width &&
-            rect1.x + rect1.width > boxes[i].x &&
-            rect1.y < boxes[i].y + boxes[i].height &&
-            rect1.height + boxes[i].y > boxes[i].y) {
-
+        if (boxes[i].xpos > 780 || boxes[i].xpos < 0) {
             boxes[i].xstep = -boxes[i].xstep;
         }
+        if (boxes[i].ypos > 580 || boxes[i].ypos < 0) {
+            boxes[i].ystep = -boxes[i].ystep;
+        }
 
-
-
-        if (rect2.x < boxes[i].x + boxes[i].width &&
-            rect2.x + rect2.width > boxes[i].x &&
-            rect2.y < boxes[i].y + boxes[i].height &&
-            rect2.height + boxes[i].y > boxes[i].y) {
-
+        if (paddlez[i].x < boxes[i].x + boxes[i].width &&
+            paddlez[i].x + paddlez[i].width > boxes[i].x &&
+            paddlez[i].y < boxes[i].y + boxes[i].height &&
+            paddlez[i].height + boxes[i].y > boxes[i].y) {
             boxes[i].xstep = -boxes[i].xstep;
-
+        }
+        if (paddles[i].x < boxes[i].x + boxes[i].width &&
+            paddles[i].x + paddles[i].width > boxes[i].x &&
+            paddles[i].y < boxes[i].y + boxes[i].height &&
+            paddles[i].height + boxes[i].y > boxes[i].y) {
+            boxes[i].xstep = -boxes[i].xstep;
         }
 
         boxes[i].xpos = boxes[i].xpos + boxes[i].xstep;
         boxes[i].ypos = boxes[i].ypos + boxes[i].ystep;
         boxes[i].render();
+        paddles[i].render();
+        paddlez[i].render();
+
+
+
     }
 }
-//--------------------------------------------------------------------------//
+
+
+
 
